08b8338693c17dd85bd999a0e7a50809e4f66b46
sujitrs <suryawanshisujeet@gmail.com>
2019-06-17 13:26:19 +0530

    init

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+	 @Value("${TARGET:World}")
+     String target;
+
+     @RestController
+     class HelloworldController {
+             @GetMapping("/")
+             String hello() {
+                     return "Hello " + target + "!";
+             }
+     }
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
